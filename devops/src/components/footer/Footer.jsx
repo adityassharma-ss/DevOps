4f274ae535e6d43b3dd35aeed75de33bce5d6196
Aditya Sharma <adityashf7@gmail.com>
2023-01-11 14:42:36 +0530

    Updated content

---
+++ devops/src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './foot.scss'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <p>Copyright © 2022 DevOps</p>
+        <p>Follow us on Social Media: <a href="#">Facebook</a> | <a href="#">Twitter</a> | <a href="#">LinkedIn</a></p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
