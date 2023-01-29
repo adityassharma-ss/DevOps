2f7ee0238ea8f75fd19469a553301735410558f8
Aditya Sharma <adityashf7@gmail.com>
2023-01-29 23:39:20 +0530

    Updated K8s

--- GO/StringArray/Array.go
+++ GO/StringArray/Array.go
@@ -3,5 +3,7 @@ import ("fmt")
 
 func main() {
   var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"}
-  fmt.Print(cars)
+  fmt.Println(cars)
+  fmt.Println(cars[0])
+  fmt.Println(cars[0] + " " + cars[1])
 }
