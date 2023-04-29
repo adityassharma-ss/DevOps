0b6c551b803984c5fde26433745f5a9c46243d9b
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2023-04-29 23:38:59 +0530

    Update Characters.go

--- GO/Main/Characters.go
+++ GO/Main/Characters.go
@@ -2,7 +2,7 @@ package main
 import "fmt"
 
 func main() {
-	var myByte byte = 'a'
+	var myByte byte = 'a' // character declared
 	var myRune rune = '♥'
 
 	fmt.Printf("%c = %d and %c = %U\n", myByte, myByte, myRune, myRune)
